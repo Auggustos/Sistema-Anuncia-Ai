989c171e14cfeec861050569d0bb5127fb656fd6
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2020-10-30 21:09:41 -0300

    !!

--- api2/src/services/CreateProductService.ts
+++ api2/src/services/CreateProductService.ts
@@ -5,6 +5,8 @@ import Product from '../models/Product';
 import uploadConfig from '../config/upload';
 
 interface Request {
+  id: string;
+  perfil: number;
   descricao: string;
   preco: number;
   id_usuario: string;
@@ -14,6 +16,8 @@ interface Request {
 }
 class CreateProductService {
   public async execute({
+    id,
+    perfil,
     descricao,
     preco,
     id_usuario,
@@ -21,6 +25,13 @@ class CreateProductService {
     nome,
     imagemFileName,
   }: Request): Promise<Product> {
+    if (perfil === 0) {
+      throw new Error('Você não é um vendedor');
+    }
+
+    if (!id) {
+      throw new Error('Você não está logado');
+    }
     const productsRepository = getRepository(Product);
 
     const product = productsRepository.create({
